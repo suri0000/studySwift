1bbc2eea73a45235fe9984b4e04fbbe005b98a44
suri0000 <yesle2005@naver.com>
2024-05-07 11:20:19 +0900

    [Feat]#3-SearchBar UI

--- BookShelf/SearchViewController.swift
+++ BookShelf/SearchViewController.swift
@@ -5,14 +5,36 @@
 //  Created by 예슬 on 5/3/24.
 //
 
+import SnapKit
 import UIKit
 
 class SearchViewController: UIViewController {
+  
+  let searchBar = UISearchBar()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    setLayout()
+    setSerchBar()
+  }
+  
+  func setSerchBar() {
+    searchBar.searchBarStyle = .minimal
+    searchBar.placeholder = "책 제목을 검색해 주세요"
+  }
+  
+  func setLayout() {
+    view.addSubview(searchBar)
+    searchBar.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(10)
+      $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(10)
+    }
   }
 
+}
 
+#Preview {
+  SearchViewController()
 }
 
