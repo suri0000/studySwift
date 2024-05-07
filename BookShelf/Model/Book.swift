bfa89a28d5de008c5b62346b7c2c87845f3a167a
suri0000 <yesle2005@naver.com>
2024-05-08 01:35:59 +0900

    [Add]#3-Model, ViewModel file

---
+++ BookShelf/Model/Book.swift
@@ -0,0 +1,50 @@
+//
+//  Book.swift
+//  BookShelf
+//
+//  Created by 예슬 on 5/8/24.
+//
+
+import Foundation
+
+//   let book = try? JSONDecoder().decode(Book.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Book
+struct Book: Codable {
+    let meta: Meta
+    let documents: [Document]
+}
+
+// MARK: - Document
+struct Document: Codable {
+    let authors: [String]
+    let contents, datetime, isbn: String
+    let price: Int
+    let publisher: String
+    let salePrice: Int
+    let status: String
+    let thumbnail: String
+    let title: String
+    let translators: [String]
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case authors, contents, datetime, isbn, price, publisher
+        case salePrice = "sale_price"
+        case status, thumbnail, title, translators, url
+    }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+    let isEnd: Bool
+    let pageableCount, totalCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case isEnd = "is_end"
+        case pageableCount = "pageable_count"
+        case totalCount = "total_count"
+    }
+}
---
+++ BookShelf/ViewModel/SearchResultViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultViewModel.swift
+//  BookShelf
+//
+//  Created by 예슬 on 5/8/24.
+//
+
+import Foundation
+
+class SearchResultViewModel {
+  
+}
