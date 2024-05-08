58781609a693d5668f46ed0ac763ea7d16ff46a2
suri0000 <yesle2005@naver.com>
2024-05-08 22:21:51 +0900

    [Feat]#5-담기 누르면 모달 내리기

--- BookShelf/View/BookDetailViewController.swift
+++ BookShelf/View/BookDetailViewController.swift
@@ -12,13 +12,20 @@ class BookDetailViewController: UIViewController {
   var document: Document?
   
   private let contentScrollView: UIScrollView = {
-          let scrollView = UIScrollView()
-          scrollView.translatesAutoresizingMaskIntoConstraints = false
-          scrollView.backgroundColor = .white
-          //scrollView.showsVerticalScrollIndicator = false
-          
-          return scrollView
-      }()
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.backgroundColor = .blue
+    //scrollView.showsVerticalScrollIndicator = false
+    
+    return scrollView
+  }()
+  
+  private let contentView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = .white
+    return view
+  }()
   
   lazy var closeButton: UIButton = {
     let button = UIButton()
@@ -32,7 +39,11 @@ class BookDetailViewController: UIViewController {
   }()
   
   lazy var addButton: UIButton = {
-    let button = UIButton()
+    let buttonConfig = UIButton.Configuration.tinted()
+    let button = UIButton(configuration: buttonConfig)
+    
+    button.setTitle("담기", for: .normal)
+    button.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
     
     return button
   }()
@@ -46,17 +57,37 @@ class BookDetailViewController: UIViewController {
   
   private func setLayout() {
     view.addSubview(contentScrollView)
+    contentScrollView.addSubview(contentView)
     view.addSubview(closeButton)
+    view.addSubview(addButton)
     
     closeButton.snp.makeConstraints { make in
       make.top.trailing.equalTo(view.safeAreaLayoutGuide).inset(10)
     }
+    
+    addButton.snp.makeConstraints { make in
+      make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+      make.bottom.equalTo(view.safeAreaLayoutGuide).inset(5)
+    }
+    
+    contentScrollView.snp.makeConstraints { make in
+      make.top.equalTo(closeButton.snp.bottom).inset(-3)
+      make.bottom.equalTo(addButton.snp.top).inset(-10)
+      make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+    }
+    
+    contentView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+      make.width.equalToSuperview()
+    }
   }
   
   @objc func closeButtonTapped() {
-      // 뷰 컨트롤러를 닫는 코드를 여기에 작성
       self.dismiss(animated: true, completion: nil)
   }
   
+  @objc func addButtonTapped() {
+      self.dismiss(animated: true, completion: nil)
+  }
   
 }
---
+++ BookShelf/View/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  BookShelf
+//
+//  Created by 예슬 on 5/8/24.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+  lazy var bookTitle: UILabel = {
+    let label = UILabel()
+    
+    label.font = UIFont.preferredFont(forTextStyle: .headline)
+    
+    return label
+  }()
+  
+  
+
+}
