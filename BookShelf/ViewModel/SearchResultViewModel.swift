d858681c133a2ac3fd4e6ed8420686c9a071ce27
suri0000 <yesle2005@naver.com>
2024-05-08 09:48:18 +0900

    [Feat]#3-API KEY 관리

--- BookShelf/ViewModel/SearchResultViewModel.swift
+++ BookShelf/ViewModel/SearchResultViewModel.swift
@@ -5,8 +5,22 @@
 //  Created by 예슬 on 5/8/24.
 //
 
-import Foundation
+import UIKit
 
 class SearchResultViewModel {
+  var bookImage: UIImage?
+  var bookTitle: String?
+  var authors: String?
   
+  func fetchSearchResult() {
+    let url = URL(string: "https://dapi.kakao.com/v3/search/book?query=세이노")!
+    
+    var request = URLRequest(url: url)
+    
+    request.allHTTPHeaderFields = ["Authorization": "KakaoAK <이 곳에 RESTAPI Key 를 넣으세요.>"]
+    
+    URLSession.shared.dataTask(with: request) { data, _, _ in
+      print(String(data: data!, encoding: .utf8))
+    }.resume()
+  }
 }
